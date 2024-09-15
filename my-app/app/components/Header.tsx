87de0d4ce239c89aa49d22916d1c8244fcc83f27
Yurii Shpakovych <Yurii.Shpakovych@davita.com>
2024-09-15 17:37:32 +0300

    Initial Commit

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -1,25 +1,22 @@
-'use client';
-
 import Link from 'next/link';
-import { usePathname } from 'next/navigation'
 
 const Header = () => {
-    const pathname = usePathname();
-    const navItems = [
-        { href: '/', text: 'Home' },
-        { href: '/about', text: 'About' },
-        { href: '/services', text: 'Services' },
-        { href: '/contact', text: 'Contact' }
-    ].map(item => (<li key={item.href}>
-        <Link className={`hover:text-gray-400 ${pathname === item.href && 'text-blue-600'}`}
-              href={item.href}>{item.text}</Link>
-    </li>));
-
     return (
         <header className="bg-gray-800 text-white">
             <nav className="container mx-auto flex justify-between items-center py-4">
                 <ul className="flex space-x-8">
-                    {navItems}
+                    <li>
+                        <Link className="hover:text-gray-400" href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link className="hover:text-gray-400" href="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link className="hover:text-gray-400" href="/services">Service</Link>
+                    </li>
+                    <li>
+                        <Link className="hover:text-gray-400" href="/contact">Contact</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
