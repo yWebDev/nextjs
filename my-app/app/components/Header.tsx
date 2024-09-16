0d7b845579049395b901b66b04c33f1b89a0bd43
Yurii Shpakovych <Yurii.Shpakovych@davita.com>
2024-09-16 10:41:48 +0300

    usePathname use-case solution

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -1,26 +1,32 @@
+'use client'
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
-    return (
-        <header className="bg-gray-800 text-white">
-            <nav className="container mx-auto flex justify-between items-center py-4">
-                <ul className="flex space-x-8">
-                    <li>
-                        <Link className="hover:text-gray-400" href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link className="hover:text-gray-400" href="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link className="hover:text-gray-400" href="/services">Service</Link>
-                    </li>
-                    <li>
-                        <Link className="hover:text-gray-400" href="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  const pathname = usePathname();
+  const navItems = [
+    { href: '/', text: 'Home' },
+    { href: '/about', text: 'About' },
+    { href: '/services', text: 'Services' },
+    { href: '/contact', text: 'Contact' },
+  ].map(({ href, text }) => (
+    <li key={href}>
+      <Link className={`hover:text-gray-400 ${pathname === href && 'text-blue-500'}`} href={href}>
+        {text}
+      </Link>
+    </li>
+  ))
+
+  return (
+    <header className="bg-gray-800 text-white">
+      <nav className="container mx-auto flex justify-between items-center py-4">
+        <ul className="flex space-x-8">
+          {navItems}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
